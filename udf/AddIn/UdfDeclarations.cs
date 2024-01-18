153f223b644e26d9f48a9f0f8496790d1b70212d
Maksim Grishkin <FFFFF@bk.ru>
2024-01-18 20:57:36 +0300

    style(excel): Метаданные функций сдвинуты к их определению

--- udf/AddIn/UdfDeclarations.cs
+++ udf/AddIn/UdfDeclarations.cs
@@ -5,11 +5,12 @@ using ExcelDna.Registration;
 namespace gmafffff.excel.udf.AddIn;
 
 public static class Функции {
-    #region Пояснительные надписи
-
     private const string МояКатегория = "Функции от gmaFFFFF";
 
-    // РублиПрописью
+    #region Форматирование текста
+
+    #region РублиПрописью
+
     private const string РПИ = nameof(РублиПрописью);
     private const string РПО = "Отображает сумму в рублях прописью";
     private const string РПАСуммаИ = "суммаРублей";
@@ -20,7 +21,19 @@ public static class Функции {
                                       "(целая/дробная часть, т - текстом, з - с заглавной, n - ширина), р/к[с]" +
                                       "(валюта базовая/дробная, с - сокращенная). Пример: «ч2 рс (бтз р д2 к)»";
 
-    // ОкруглГаус
+    [ExcelFunction(Name = РПИ, Category = МояКатегория, Description = РПО, IsThreadSafe = true)]
+    public static string РублиПрописью(
+        [ExcelArgument(Name = РПАСуммаИ, Description = РПАСуммаО)]
+        double сумма,
+        [ExcelArgument(Name = РПАФорматИ, Description = РПAФорматО)]
+        string формат = "") {
+        return ДеньгиПрописью.ДеньгиПрописью.РублиПрописью(сумма, формат);
+    }
+
+    #endregion
+
+    #region ОкруглГаус
+
     private const string ОГИ = nameof(ОкруглГаус);
     private const string ОГО = "Округление по Гауссу до ближайшего четного знака";
     private const string ОГАЧислоИ = "число";
@@ -30,37 +43,27 @@ public static class Функции {
     private const string ОГАЗнаковО = "число знаков, до которых происходит округление. Если < 0, то перед запятой. " +
                                       "Максимум 15 знаков, по умолчанию — 0";
 
-    // JSONPath, JMESPath, JsonИндекс
-    private const string JsonPathHelpUrl =
-        "https://danielaparker.github.io/JsonCons.Net/articles/JsonPath/JsonConsJsonPath.html";
+    [ExcelFunction(Name = ОГИ, Category = МояКатегория, Description = ОГО, IsThreadSafe = true)]
+    public static double ОкруглГаус(
+        [ExcelArgument(Name = ОГАЧислоИ, Description = ОГАЧислоО)]
+        double число,
+        [ExcelArgument(Name = ОГАЗнаковИ, Description = ОГАЗнаковО)]
+        short знаков) {
+        return (знаков, Math.Pow(10, -знаков)) switch {
+            (> 15, _) => Math.Round(число, 15, MidpointRounding.ToEven),
+            (>= 0, _) => Math.Round(число, знаков, MidpointRounding.ToEven),
+            (< 0, var степень) => Math.Round(число / степень, 0, MidpointRounding.ToEven) * степень
+        };
+    }
 
-    private const string JmesPathHelpUrl = "https://jmespath.org/specification.html";
+    #endregion
 
-    private const string JИИ = nameof(JsonИндекс);
-    private const string JPИ = nameof(JsonPath);
-    private const string JmPИ = nameof(JmesPath);
+    #endregion
 
-    private const string JИО = "Извлекает элементы json по индексу";
+    #region Http
 
-    private const string JPО = "Извлекает элементы json с помощью синтаксиса JSONPath. " +
-                               "Не умеет проецировать данные (например, фильтрация с последующим индексом массива). " +
-                               "При необходимости проецировать данные используйте функцию JmesPath.\n" +
-                               "Примеры запросов в справке";
+    #region Get/Post
 
-    private const string JmPО = "Извлекает элементы json с помощью синтаксиса JMESPath.\n" +
-                                "Примеры запросов в справке";
-
-    private const string JPJMАJsonТекстИ = "jsonТекст";
-    private const string JPJMАJsonТекстО = "Json документ";
-
-    private const string JИАИндексИ = "индекс";
-    private const string JИАИндексО = "индексы для доступа к JSON";
-    private const string JPАJsonPathИ = "jsonPath";
-    private const string JPАJsonPathО = "JSONPath, подробнее о формате в справке к функции";
-    private const string JmPАJsonPathИ = "jmesPath";
-    private const string JmPАJsonPathО = "JMESPath, подробнее о формате в справке к функции";
-
-    // HttpGet и HttpPost
     private const string HGАктивИ = nameof(HttpGet_active);
     private const string HPАктивИ = nameof(HttpPost_active);
 
@@ -99,49 +102,6 @@ public static class Функции {
 
     private const string HPАТелоИ = "тело";
     private const string HPАТелоО = "Тело запроса в формате json (необязательно)";
-
-    // Base64Кодировать и Base64Декодировать
-    private const string B64КИ = nameof(Base64Кодировать);
-    private const string B64ДИ = nameof(Base64Декодировать);
-
-    private const string B64КО = "Кодирует текст в base64 код";
-    private const string B64ДО = "Декодирует текст из формата base64";
-
-    private const string B64КАТекстИ = "текст";
-    private const string B64КАТекстО = "текст для кодирования";
-
-    private const string B64ДАbase64ТекстИ = "base64Текст";
-    private const string B64ДАbase64ТекстО = "текст закодированный в формате base64";
-
-    #endregion
-
-    #region Без группировки
-
-    [ExcelFunction(Name = РПИ, Category = МояКатегория, Description = РПО, IsThreadSafe = true)]
-    public static string РублиПрописью(
-        [ExcelArgument(Name = РПАСуммаИ, Description = РПАСуммаО)]
-        double сумма,
-        [ExcelArgument(Name = РПАФорматИ, Description = РПAФорматО)]
-        string формат = "") {
-        return ДеньгиПрописью.ДеньгиПрописью.РублиПрописью(сумма, формат);
-    }
-
-    [ExcelFunction(Name = ОГИ, Category = МояКатегория, Description = ОГО, IsThreadSafe = true)]
-    public static double ОкруглГаус(
-        [ExcelArgument(Name = ОГАЧислоИ, Description = ОГАЧислоО)]
-        double число,
-        [ExcelArgument(Name = ОГАЗнаковИ, Description = ОГАЗнаковО)]
-        short знаков) {
-        return (знаков, Math.Pow(10, -знаков)) switch {
-            (> 15, _) => Math.Round(число, 15, MidpointRounding.ToEven),
-            (>= 0, _) => Math.Round(число, знаков, MidpointRounding.ToEven),
-            (< 0, var степень) => Math.Round(число / степень, 0, MidpointRounding.ToEven) * степень
-        };
-    }
-
-    #endregion
-
-    #region Http
 
     [ExcelAsyncFunction(Name = HGАктивИ, Category = МояКатегория, Description = HGАктивО)]
     public static async Task<object> HttpGet_active(
@@ -202,6 +162,39 @@ public static class Функции {
         return JsonКлиент.JsonКлиент.JsonPathНайди(ответ, jsonPath);
     }
 
+    #endregion
+
+    #region Json // JSONPath, JMESPath, JsonИндекс
+
+    private const string JsonPathHelpUrl =
+        "https://danielaparker.github.io/JsonCons.Net/articles/JsonPath/JsonConsJsonPath.html";
+
+    private const string JmesPathHelpUrl = "https://jmespath.org/specification.html";
+
+    private const string JИИ = nameof(JsonИндекс);
+    private const string JPИ = nameof(JsonPath);
+    private const string JmPИ = nameof(JmesPath);
+
+    private const string JИО = "Извлекает элементы json по индексу";
+
+    private const string JPО = "Извлекает элементы json с помощью синтаксиса JSONPath. " +
+                               "Не умеет проецировать данные (например, фильтрация с последующим индексом массива). " +
+                               "При необходимости проецировать данные используйте функцию JmesPath.\n" +
+                               "Примеры запросов в справке";
+
+    private const string JmPО = "Извлекает элементы json с помощью синтаксиса JMESPath.\n" +
+                                "Примеры запросов в справке";
+
+    private const string JPJMАJsonТекстИ = "jsonТекст";
+    private const string JPJMАJsonТекстО = "Json документ";
+
+    private const string JИАИндексИ = "индекс";
+    private const string JИАИндексО = "индексы для доступа к JSON";
+    private const string JPАJsonPathИ = "jsonPath";
+    private const string JPАJsonPathО = "JSONPath, подробнее о формате в справке к функции";
+    private const string JmPАJsonPathИ = "jmesPath";
+    private const string JmPАJsonPathО = "JMESPath, подробнее о формате в справке к функции";
+
     [ExcelFunction(Name = JИИ, Category = МояКатегория, Description = JИО, IsThreadSafe = true)]
     public static object JsonИндекс(
         [ExcelArgument(Name = JPJMАJsonТекстИ, Description = JPJMАJsonТекстО)]
@@ -230,6 +223,23 @@ public static class Функции {
         return JsonКлиент.JsonКлиент.JmesPathИзмени(jsonТекст, jmesPath);
     }
 
+    #endregion
+
+    #region Base64
+
+    // Base64Кодировать и Base64Декодировать
+    private const string B64КИ = nameof(Base64Кодировать);
+    private const string B64ДИ = nameof(Base64Декодировать);
+
+    private const string B64КО = "Кодирует текст в base64 код";
+    private const string B64ДО = "Декодирует текст из формата base64";
+
+    private const string B64КАТекстИ = "текст";
+    private const string B64КАТекстО = "текст для кодирования";
+
+    private const string B64ДАbase64ТекстИ = "base64Текст";
+    private const string B64ДАbase64ТекстО = "текст закодированный в формате base64";
+
     [ExcelFunction(Name = B64КИ, Category = МояКатегория, Description = B64КО, IsThreadSafe = true)]
     public static string Base64Кодировать(
         [ExcelArgument(Name = B64КАТекстИ, Description = B64КАТекстО)]
@@ -245,6 +255,8 @@ public static class Функции {
         var байты = Convert.FromBase64String(base64Текст);
         return Encoding.UTF8.GetString(байты);
     }
+
+    #endregion
 
     #endregion
 }
